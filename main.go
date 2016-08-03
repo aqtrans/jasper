55e2b583b841b2d95d18f42cad919ddf2137fce3
Jordan Anderson <me@jba.io>
2016-08-02 22:43:33 -0500

    Clean up and comment stuff

--- main.go
+++ main.go
@@ -19,13 +19,12 @@ import (
 	"golang.org/x/image/font"
 	"net/http"
 	"github.com/dimfeld/httptreemux"
-	//"image/color"
-	//"fmt"
 )
 
 func drawHandler(w http.ResponseWriter, r *http.Request) {
 	params := r.Context().Value(httptreemux.ParamsContextKey).(map[string]string)
 	ptext := params["text"]
+	// Add a question mark to the end of given text
 	text := ptext + "?"
 	title := "That's a Paddlin'"
 	log.Println(text)
@@ -74,16 +73,13 @@ func drawHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	d.DrawString(title)
 	
-	//dy := int(math.Ceil(12 * 1.0 * 72 / 72))
-
+	// Now we setup and draw the given text
 	dm := d.MeasureString(text)
 	textWidth := dm.Round()
 	imageWidth := b.Max.X
-	log.Println("textWidth")
-	log.Println(textWidth)
-	log.Println("imageWidth")
-	log.Println(imageWidth)
-		
+	
+	// If the width of the text is wider than the image, 
+	// we loop through shrinking the font size until the text fits
 	for textWidth > imageWidth {
 		log.Println("Text too long")
 		fontSize = fontSize-1.0
@@ -111,12 +107,8 @@ func drawHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	d.DrawString(text)
 
-	//toimg, _ := os.Create("omg.png")
-	//defer toimg.Close()
-
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "image/png")
-
 
 	err = png.Encode(w, newimage)
 	if err != nil {
@@ -130,82 +122,5 @@ func main() {
 	r.GET("/*text", drawHandler)
 	http.Handle("/", r)
 
-	http.ListenAndServe("0.0.0.0:3002", nil)
-
-
-	//var text = string("OMG YEAH WHAT OMG YEAH WHAT WHAT WHAT WHAT WHAT WHAT WHAT WHAT WHAT WHAT")
-
-
-	/*
-	y += dy
-	for _, s := range text {
-		d.Dot = fixed.P(10, y)
-		d.DrawString(s)
-		y += dy
-	}
-	*/
-
-	// The below works, printing a simple OMG on the image
-	// But it does not calculate the width at all
-	/* 
-	c := freetype.NewContext()
-	c.SetDPI(72)
-	c.SetFont(myFont)
-	//c.SetSrc(m)
-	c.SetSrc(image.Black)
-	c.SetDst(newimage)
-	c.SetFontSize(20)
-	c.SetClip(originalimage.Bounds())
-	imgP := image.Point{0, originalimage.Bounds().Dy()-10}
-	pt := freetype.Pt(imgP.X, imgP.Y)
-	c.DrawString(text, pt)
-	*/
-
-	//c.SetClip(m.Bounds())
-	/*
-	pt := freetype.Pt(0, b.Dy())
-	_, err = c.DrawString("OMGGGG", pt)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	toimg, _ := os.Create("omg.png")
-	defer toimg.Close()
-	png.Encode(toimg, newimage)*/
-
-	// http://stackoverflow.com/questions/29105540/aligning-text-in-golang-with-truetype
-    // Draw the guidelines.
-	/*
-    ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
-    for rcount := 0; rcount < 4; rcount ++ {
-        for i := 0; i < 200; i++ {
-            newimage.Set(250*rcount, i, ruler)
-        }
-    }
-
-    // Truetype stuff
-    opts := truetype.Options{}
-    opts.Size = 100.0
-    face := truetype.NewFace(font, &opts)
-
-
-    // Calculate the widths and print to image
-    for i, x := range(text) {
-        awidth, ok := face.GlyphAdvance(rune(x))
-        if ok != true {
-            log.Println(err)
-            return
-        }
-        iwidthf := int(float64(awidth) / 64)
-        //fmt.Printf("%+v\n", iwidthf)
-
-        pt := freetype.Pt(i*250+(125-iwidthf/2), 128)
-        c.DrawString(string(x), pt)
-        log.Println(string(x))
-        //fmt.Printf("%+v\n", awidth)
-    }
-	*/
-
-
-
+	http.ListenAndServe("127.0.0.1:3002", nil)
 }
